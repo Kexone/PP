8aa4c46de5f1f5bec0cf903469bbc459bf883465
Kexone <kami.extreme@gmail.com>
2017-10-17 09:20:57 +0200

    fix cosmetic bugs

--- hw/filterW.hs
+++ hw/filterW.hs
@@ -1,12 +1,16 @@
-module FIlterWords where
+module FilterWords where
 
 import Prelude
 import Data.List
 
 type Index = [(String, [Int])]
 type LocatedWords = [(Int, [String])]
+
 txt = "In computer science, functional programming is a programming\nparadigm that treats computation as the evaluation of mathematical\nfunctions and avoids state and mutable data. It emphasizes the\napplication of functions in contrast tothe imperative programming style,\nwhich emphasizes changes in state"
 
+addGap :: Int -> String
+addGap 0 = ""
+addGap len =  " " ++ addGap (len-1)
 
 sortWords :: (Ord a) => [a] -> [a]
 sortWords = map head . group . sort
@@ -19,7 +23,7 @@ searchWord word text = [x | (x, y) <- text, contains word y]
 
 printText :: Index -> String
 printText [] = ""
-printText ((word, numb):xs) = ( "  " ++ word ++ " " ++   (show numb) ++ "\n") ++ (printText xs)
+printText ((word, numb):xs) = ( " " ++ word ++ addGap (15 - (length word)) ++   (show numb) ++ "\n") ++ (printText xs)
 
 contains :: Eq a => a -> [a] -> Bool
 contains elem [] = False
@@ -34,8 +38,8 @@ prepare str = zip [1..(length linesList)] linesList where
     linesList = map words (clearCh ',' (clearCh '.' (lines str)))
 
 getIndex :: String -> Int -> Index
-getIndex text len = [(word, searchWord word preparedText) | word <- getWords text len]  where
-	preparedText = prepare text
+getIndex text len = [(word, searchWord word filteredTxt) | word <- getWords text len]  where
+	filteredTxt = prepare text
 
 --print anything
 printThis :: String -> IO()
@@ -43,11 +47,11 @@ printThis = putStr
 
 --print original text
 pOT :: IO()
-pOT = putStr ("\n\nOriginal text:\n\n" ++ txt ++ "\n\n" )
+pOT = putStr ("\n\nText:\n\n" ++ txt ++ "\n\n" )
 
 --print words with index
 pI :: Int -> IO()
-pI len= putStr ("\n\n" ++ printText (getIndex txt len) ++ "\n\n" )
+pI len= putStr ("\n\n  Words:\n\n" ++ printText (getIndex txt len) ++ "\n\n" )
 
 --showIndex = putStr ("\nIndex:\n\n" ++ printText (buildIndex txt (prepare txt)) ++ "\n\nOriginal text:\n\n" ++ txt ++ "\n")
 
