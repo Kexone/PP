25a91b437803d5660714b8198a6934528139c163
Kexone <kami.extreme@gmail.com>
2017-11-22 02:55:00 +0100

    fix print

--- hw/multiplicationMatrix/multiplicationMatrix/main.cpp
+++ hw/multiplicationMatrix/multiplicationMatrix/main.cpp
@@ -8,11 +8,11 @@ int main()
 {
 	int n = mp.getN();
 	std::cout << "Size of matrices: " << n << " x " << n << std::endl << std::endl;
-	std::cout << "\n\nCalc multiplicaton matrices in 1D arrays" << std::endl << std::endl;
+	std::cout << "\n\nCalc multiplication matrices in 1D arrays" << std::endl << std::endl;
 	mp.calculate1DArray();
-	std::cout << "\n\nCalc multiplicaton matrices in 2D arrays" << std::endl << std::endl;
+	std::cout << "\n\nCalc multiplication matrices in 2D arrays" << std::endl << std::endl;
 	mp.calculate2DArray();
-	std::cout << "\n\nCalc multiplicaton matrices in vectors" << std::endl << std::endl;
+	std::cout << "\n\nCalc multiplication matrices in vectors" << std::endl << std::endl;
 	mp.calculateVectors();
 	getchar();
 	return 0;
